831e98c3f2d2bae7538c08febb95df78c9ebe150
Chris Taylor <1373858+sciencectn@users.noreply.github.com>
2020-04-08 14:08:15 -0400

    changed name to avoid conflicts, added more info

--- setup.py
+++ setup.py
@@ -6,10 +6,13 @@ simutils: numpy and timing tools to help with creating simulations
 """
 
 setup(
-    name="simtools",
-    version="0.0",
+    name="particle-simtools",
+    author="Chris Taylor",
+    author_email="sciencectn@gmail.com",
+    url="https://github.com/sciencectn/simtools",
+    description="A collection of functions to help with simulating particle models",
+    version="0.0.1",
     packages=find_packages(),
     install_requires=["pyquaternion"]
 )
-
 
